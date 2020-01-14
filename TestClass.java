e7994b96837070efbfdaf194313d6b140df94eb2
nhy31 <59862742+nhy31@users.noreply.github.com>
2020-01-14 16:08:30 +0900

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -4,6 +4,6 @@ public class TestClass {
 
     public  static  void main (String[] args) {
         System.out.println("Hi^^");
-        System.out.println("coding!");
+        System.out.println("coding!~~");
     }
 }
