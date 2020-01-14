05fc71054da1fa1c3d062b95c6b045f2a38c0f38
nhy <nahay131@gmail.com>
2020-01-14 15:05:12 +0900

    test commit

--- TestClass.java
+++ TestClass.java
@@ -1,6 +1,8 @@
+package test;
+
 public class TestClass {
 
-    public void static main (String[] args) {
-        System.out.println("Hello!");
+    public  static  void main (String[] args) {
+        System.out.println("Hello!!");
     }
 }
